70995aa92c59ce380cbdbd2b175dffd809ca96c0
MCC <scriptchuang@163.com>
2021-04-20 10:37:06 +0800

    1,整理并增加element联动地址组件的数据；2，开票功能相关

--- src/components/OrderRenewal/OrderLists.vue
+++ src/components/OrderRenewal/OrderLists.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="order-box" v-loading="loading">
+    <div class="order-box">
         
 		<el-row>
 			<el-button type="primary" class="btn-blue" @click="sendFn">批量开票</el-button>
@@ -10,6 +10,72 @@
         <el-pagination @current-change="searchFn" :current-page="searchLists.page" @size-change="sizeChange"
 		:page-size="searchLists.page_size" :page-sizes="pageSizes" layout="sizes, total, prev, pager, next, jumper" :total="total">
 		</el-pagination>
+        <!-- 弹框 -->
+        <el-dialog class="max-dialog" :title="dalogTitle" :visible.sync="centerDialogVisible" center width='530px'>
+            <el-form :model="editData" :validate-on-rule-change="false" :rules="rules" ref="form" label-width="135px">
+                <el-form-item label="开票类型" prop="out_type">
+                    <el-radio-group v-model="editData.out_type">
+                        <el-radio :label="1">纸质发票</el-radio>
+                        <el-radio :label="2">电子发票</el-radio>
+                    </el-radio-group>
+                </el-form-item>
+                <el-form-item label="公司抬头" prop="title_name">
+                    <el-input v-model="editData.title_name" autocomplete="off" clearable></el-input>
+                </el-form-item>
+                <el-form-item label="公司税号" prop="tax_number">
+                    <el-input v-model="editData.tax_number" autocomplete="off" clearable></el-input>
+                </el-form-item>
+                <div v-show="editData.out_type === 2">
+                    <el-form-item label="邮箱" prop="user_email">
+                        <el-input v-model="editData.user_email" autocomplete="off" clearable></el-input>
+                    </el-form-item>
+                    <el-form-item label="手机号" prop="user_mobile">
+                        <el-input v-model="editData.user_mobile" autocomplete="off" clearable></el-input>
+                    </el-form-item>
+                </div>
+                <div v-show="editData.out_type === 1">
+                    <el-form-item label="收件人" prop="user_name">
+                        <el-input v-model="editData.user_name" autocomplete="off" clearable></el-input>
+                    </el-form-item>
+                    <el-form-item label="收件人电话" prop="user_phone">
+                        <el-input v-model="editData.user_phone" autocomplete="off" clearable></el-input>
+                    </el-form-item>
+                    <el-form-item label="邮寄地址" prop="user_region">
+
+                        <el-cascader
+                            placeholder="地址选择"
+                            :options="distData"
+                            filterable
+                            v-model="editData.user_region"
+                            class="dist-style"
+                        >
+                            
+                        </el-cascader>
+
+
+                    </el-form-item>
+                    <el-form-item label="邮寄详细地址" prop="user_address">
+                        <el-input v-model="editData.user_address" autocomplete="off" clearable></el-input>
+                    </el-form-item>
+                </div>
+                <el-form-item label="开户银行" prop="bank_name">
+                    <el-input v-model="editData.bank_name" autocomplete="off" clearable></el-input>
+                </el-form-item>
+                <el-form-item label="开户账号" prop="bank_account">
+                    <el-input v-model="editData.bank_account" autocomplete="off" clearable></el-input>
+                </el-form-item>
+                <el-form-item label="单位地址" prop="address">
+                    <el-input v-model="editData.address" autocomplete="off" clearable></el-input>
+                </el-form-item>
+                <el-form-item label="单位电话" prop="phone">
+                    <el-input v-model="editData.phone" autocomplete="off" clearable></el-input>
+                </el-form-item>
+                
+            </el-form>
+            <span slot="footer" class="dialog-footer">
+                <el-button type="primary" class="btn-dialog" @click="createAn('form')">确 定</el-button>
+            </span>
+        </el-dialog>
     </div>
 </template>
 
@@ -17,6 +83,7 @@
 <script>
 import GridManager from 'gridmanager-vue';
 import 'gridmanager-vue/css/gm-vue.css';
+import distData from './../../../static/js/city/dist'
 export default {
     props: {
         itemData:{}
@@ -25,10 +92,76 @@ export default {
         GridManager
     },
     computed: {
-
+        // 验证
+        rules(){
+            return {
+                title_name: [{
+                    required: true,
+                    message: '请正确输入【公司抬头】',
+                    trigger: 'blur'
+                }],
+                tax_number: [{
+                    required: true,
+                    message: '请正确输入【公司税号】',
+                    trigger: 'blur'
+                }],
+                user_email: [{
+                    required: this.editData.out_type === 2,
+                    message: '请正确输入【电子邮箱】',
+                    trigger: 'blur'
+                }],
+                user_mobile: [{
+                    required: this.editData.out_type === 2,
+                    message: '请正确输入【手机号码】',
+                    trigger: 'blur'
+                }],
+                user_name: [{
+                    required: this.editData.out_type === 1,
+                    message: '请正确输入【收件人】',
+                    trigger: 'blur'
+                }],
+                user_phone: [{
+                    required: this.editData.out_type === 1,
+                    message: '请正确输入【收件人电话】',
+                    trigger: 'blur'
+                }],
+                user_region: [{
+                    required: this.editData.out_type === 1,
+                    message: '请正确选择【邮寄地址】',
+                    trigger: 'blur'
+                }],
+                user_address: [{
+                    required: this.editData.out_type === 1,
+                    message: '请正确输入【详细地址】',
+                    trigger: 'blur'
+                }]
+            }
+        }
     },
     data(){
         return {
+            distData: [],
+            // 开票的数据
+            editData: {
+                out_type : 2, //  int	开票类型：1纸质发票，2电子发票
+                title_name : "", //	string	公司抬头
+                tax_number : "", // string	公司税号
+                order_ids : [], // array	开具发票的订单id
+                bank_name : "", // string	开户银行 选填
+                bank_account : "", // string	开户账号 选填
+                address : "", // string	单位地址 选填
+                phone : "", // 单位电话 选填
+                user_email : "", // string	电子发票 邮箱
+                user_mobile : "", // string	电子发票 手机号
+                user_name : "", // string	纸质发票 收件人
+                user_phone : "", // string	纸质发票 收件人电话
+                user_region : [], // array	纸质发票邮寄地址 地区码
+                user_address : "", // string	纸质发票 详细地址
+            },
+            // 弹框显示
+            centerDialogVisible: false,
+            // 弹框标题
+            dalogTitle: "",
             // 总数
             total: 0,
             // 列表查询数据
@@ -176,8 +309,46 @@ export default {
         }
     },
     methods: {
-        // 操作里面的按钮type: 1查看、2申请、3去支付(订单详情)、4删除
-        operateFn(type,data){
+        // 开票数据初始化
+        billInitFn(){
+            this.editData = {
+                out_type : 2, //  int	开票类型：1纸质发票，2电子发票
+                title_name : "", //	string	公司抬头
+                tax_number : "", // string	公司税号
+                order_ids : [], // array	开具发票的订单id
+                bank_name : "", // string	开户银行 选填
+                bank_account : "", // string	开户账号 选填
+                address : "", // string	单位地址 选填
+                phone : "", // 单位电话 选填
+                user_email : "", // string	电子发票 邮箱
+                user_mobile : "", // string	电子发票 手机号
+                user_name : "", // string	纸质发票 收件人
+                user_phone : "", // string	纸质发票 收件人电话
+                user_region : [], // array	纸质发票邮寄地址 地区码
+                user_address : "", // string	纸质发票 详细地址
+            }
+        },
+        
+        // 开票确定
+        createAn(formName){
+            let that = this;
+            that.$refs[formName].validate((valid)=>{
+                if(valid){
+                    this.operatePostFn(2,{
+                        url: "/custom/invoice/apply",
+                        option: this.editData
+                    })
+                }else{
+                    return false
+                }
+            })
+        },
+        /* 操作里面的按钮
+        type: 1查看、2申请、3去支付(订单详情)、4删除
+        data: 当前行数据
+        sendData: 发送的数据(外部按钮操作调用这个方法时会传该参数)
+        */
+        operateFn(type,data,sendData){
 
             let url = "",
                 option = {};
@@ -186,7 +357,10 @@ export default {
                     url = "/custom/invoice/detail"
                     break;
                 case 2:
-                    url = "/custom/invoice/apply"
+                    this.billInitFn()
+                    this.dalogTitle = "申请发票"
+                    this.editData.order_ids = sendData ? sendData : [data.id]
+                    this.centerDialogVisible = true
                     break;
                 case 3:
                     // url = "/custom/order/detail"
@@ -232,6 +406,8 @@ export default {
                             break;
                         case 2:
                             // 申请
+                            this.centerDialogVisible = false
+                            this.searchFn()
                             break;
                         case 3:
                             // 去支付
@@ -262,7 +438,11 @@ export default {
               this.$message.error('请至少勾选一项');
               return
           }
-          console.log(checkData)
+          let arr = []
+          checkData.map((v)=>{
+              arr.push(v.id)
+          })
+            this.operateFn(2,null,arr)
         },
         // 解除绑定
         delRelation: function(row) {
@@ -280,7 +460,9 @@ export default {
                 url = "/custom/order/index";
                 console.log(size)
             this.searchLists.page = size || 1
-            this.$axios.get(url).then((res)=>{
+            this.$axios.get(url,{
+                params: this.searchLists
+            }).then((res)=>{
                 if(res.data.code === 1){
                     GridManager.setAjaxData('girdTable', {data: res.data.data.list});
                     that.total = res.data.data.total
@@ -304,7 +486,7 @@ export default {
     },
     mounted(){
         this.searchFn()
-        
+        this.distData = distData.distData
     }
 }
 </script>
@@ -323,8 +505,10 @@ export default {
 				display: none;
 			}
 		}
+        .dist-style{
+            display: block;
+        }
 	}
-
 
     /deep/ .grid-table{
         border-bottom: #ebeef5 1px solid;
